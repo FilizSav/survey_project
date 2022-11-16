40a2c50892c85d0fad0a146e604a5a808f9d90f3
Filiz Sav <filizsav@Filizs-MacBook-Pro-2.local>
2022-11-16 16:53:07 -0600

    survey

---
+++ src/survey/Participant.java
@@ -0,0 +1,55 @@
+package survey;
+
+public class Participant {
+
+    public Participant(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    /*
+    Declare static instance variables to store below information:
+    total number of participants -> variable name can be totalNumberOfParticipants
+    total number of male participants -> variable name can be totalNumberOfMaleParticipants
+    total number of female participants -> variable name can be totalNumberOfFemaleParticipants
+    - THESE FIELDS ARE STATIC BC THE ARE FEATURES OF THE PARTICIPANTS CLASS
+    */
+
+    public static int totalNumberOfParticipants;
+    public static int totalNumberOfMaleParticipants;
+    public static int totalNumberOfFemaleParticipants;
+
+    /*
+    Declare instance variables for each participant as below
+    name -> can be String
+    age -> can be int
+    gender -> can be char
+     */
+    public String name;
+    public int age;
+    public String gender;
+
+    public Participant() {
+
+    }
+
+    //Create a static void method addParticipants() that will increase totalNumberOfParticipants by 1
+    public static void addParticipants(){totalNumberOfParticipants++;}
+
+    //Create a static void method addMaleParticipants() that will increase totalNumberOfMaleParticipants by 1
+    public static void addMaleParticipants(){totalNumberOfMaleParticipants++;}
+
+    //Create a static void method addFemaleParticipants() that will increase totalNumberOfFemaleParticipants by 1
+    public static void addFemaleParticipants(){totalNumberOfFemaleParticipants++;}
+
+    //Override toString() method and implement a new body that will return all information about a Participant
+
+    @Override
+    public String toString() {
+        return "Participant{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ src/survey/Survey.java
@@ -0,0 +1,58 @@
+package survey;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Survey {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Participant> participants = new ArrayList<>();
+
+        do{
+            System.out.println(SurveyQuestions.askToJoin);
+            String join = scan.nextLine();
+            if(join.toLowerCase().equals("y")) {
+                Participant.addParticipants();
+
+                System.out.println(SurveyQuestions.askName);
+                String name = scan.nextLine();
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = scan.nextInt();
+                scan.nextLine();
+
+                System.out.println(SurveyQuestions.askGender);
+                String gender = scan.nextLine();
+
+            Participant participant = new Participant(name,age,gender);
+            participants.add(participant);
+
+            if(gender.toLowerCase().equals("m")) Participant.addMaleParticipants();
+            else Participant.addFemaleParticipants();
+            }
+        }while (Participant.totalNumberOfParticipants < 3); System.out.println("End of Survey");
+
+        System.out.println(participants);
+        System.out.println("Total number of male participants = " + Participant.totalNumberOfMaleParticipants);
+        System.out.println("Total number of female participants = " +Participant.totalNumberOfFemaleParticipants);
+
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (Participant p : participants) {
+            if(p.age > max) max = p.age;
+            if(p.age < min ) min = p.age;
+        }
+        System.out.println("The age of the youngest person is " + min);
+        System.out.println("The age of the eldest person is " + max);
+
+    }
+            /*
+        Write a program that will get information from 3 participants and
+        -Print information for each participant
+        -Print how many participants were male
+        -Print how many participants were female
+        -Print the age of the youngest person
+        -Print the age of the eldest person
+         */
+
+}
